747bee271670f221cb3f57f5b502873b70dab08d
MarkusHolmstrom <markusholmstrom@live.se>
2022-02-17 15:59:44 +0100

    lastdigitattempt

--- CelsiusToFahrenheit/CelsiusToFahrenheit.cpp
+++ CelsiusToFahrenheit/CelsiusToFahrenheit.cpp
@@ -5,6 +5,7 @@
 #include <string> // https://www.programiz.com/cpp-programming/string-float-conversion string to double
 using namespace std;
 
+// check if there is any letters in the input
 bool isNumber(string str) {
     for (int i = 0; i < (int)str.length(); i++) {
         if (!isdigit(str[i])) {
@@ -34,13 +35,3 @@ int main()
     cin >> input;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ LastDigit/LastDigit.cpp
@@ -0,0 +1,31 @@
+// LastDigit.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+//int separateNumbers(string numbers, int index) {
+//    for (size_t i = 0; i < (int)numbers.length(); i++)
+//    {
+//        if (index == i)
+//        {
+//            return (int)numbers[i];
+//        }
+//    }
+//    return 0;
+//}
+
+int main()
+{
+    int numbers;
+    cout << "Enter number!\n";
+    cin >> numbers;
+
+
+
+    for (int value : numbers) {
+        cout << value " " << endl;
+    }
+
+}
+
